de04ad329ae1542acc0f51857a2783ace02365c3
GOVIND-PATEL-NDP <156649901+GOVIND-PATEL-NDP@users.noreply.github.com>
2025-01-08 20:27:28 +0530

    data saved

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Your db.json file
+const router = jsonServer.router('ecommerce.json'); // Your db.json file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
